12e7ec7e84596eb7025906198b553d292dc30665
davidzz <david_zipkin@yahoo.com>
2015-04-08 15:30:59 -0400

    New files

--- src/mbta2/Driver.java
+++ src/mbta2/Driver.java
@@ -1,10 +1,23 @@
 package mbta2;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Driver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
-		System.out.println ("yo");
+		
+		RESTCaller rc = new RESTCaller ();
+		URL url = constructTestURL ();
+		rc.processRESTcall (url);
+	}
+	
+	private static URL constructTestURL () throws MalformedURLException
+	{
+		String tempString = "http://realtime.mbta.com/developer/api/v2/routesbystop?api_key=wX9NwuHnZU2ToO7GmGR9uw&stop=place-bbsta&format=json";
+		URL url = new URL(tempString);
+		return url;
 	}
 
 }
---
+++ src/mbta2/RESTCaller.java
@@ -0,0 +1,12 @@
+package mbta2;
+
+import java.net.URL;
+import com.fasterxml.jackson.core.*;
+public class RESTCaller {
+
+	public void processRESTcall(URL url) {
+		// TODO Auto-generated method stub
+		// http://wiki.fasterxml.com/JacksonInFiveMinutes
+	}
+
+}
